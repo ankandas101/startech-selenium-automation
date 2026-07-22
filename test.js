49a6d8ea4ae601ea3abeb91d470068c150e4ca5c
Ankan Das <ankan.cse22@gmail.com>
2026-07-22 23:47:23 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,74 @@
+import { Browser, Builder, By, Key, until } from 'selenium-webdriver';
+import { Select } from 'selenium-webdriver/lib/select.js'
+import { faker } from '@faker-js/faker';
+
+const driver = new Builder().forBrowser(Browser.CHROME).build();
+
+const searchProduct = 'walton laptop';
+const quantity = 10;
+
+const randomFirstName = faker.person.firstName();
+const randomLastName = faker.person.lastName();
+const randomEmail = faker.internet.email();
+const randomPhone = faker.string.numeric('017########');
+const randomAddress = faker.location.streetAddress();
+const randomAddressCity = faker.location.city();
+const develeryLocation = 'Bhola';
+
+
+
+
+async function testRun(){
+
+    await driver.get("https://www.startech.com.bd");
+    await driver.manage().window().maximize();
+    await driver.sleep(2000);
+
+    //Make search operation
+    await driver.findElement(By.xpath("//input[@placeholder='Search']")).click();
+    await driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys(searchProduct);
+    await driver.sleep(3500);
+
+   await driver.findElement(By.xpath("//div[@id='tab-prod']/div[@class='search-item'][1]")).click();
+   
+   //set quantity
+   await driver.findElement(By.id("input-quantity")).clear(); 
+   await driver.findElement(By.id("input-quantity")).sendKeys(quantity);
+   await driver.findElement(By.id("button-cart")).click();
+   await driver.sleep(3000);
+
+   await driver.findElement(By.xpath("//div[@class='popup-inner']//div[@class='btn-wrap']//button[contains(text(),'View Cart')]")).click();
+   await driver.findElement(By.xpath("//a[contains(text(),'Checkout')]")).click();
+
+   //set checkout form
+   await driver.findElement(By.xpath("//div[@class='form-group']/input[@name='firstname']")).sendKeys(randomFirstName);
+   await driver.findElement(By.xpath("//div[@class='form-group']/input[@name='lastname']")).sendKeys(randomLastName);
+   await driver.findElement(By.xpath("//div[@class='form-group']/input[@name='address_1']")).sendKeys(randomAddress);
+   await driver.findElement(By.xpath("//div[@class='form-group']/input[@name='city']")).sendKeys(randomAddressCity);
+   await driver.findElement(By.xpath("//div[@class='snap-drpd-sel' and @tabindex='0']")).click();
+   await driver.sleep(300);
+   let setDeveveryLocation = await driver.findElement(By.xpath("//div[@class='snap-drpd-inn']/div[contains(text(),'Bhola' )]"));
+   await setDeveveryLocation.click();
+
+   await driver.findElement(By.xpath("//div[@class='form-group']/input[@name='telephone']")).sendKeys(randomPhone);
+   await driver.findElement(By.xpath("//div[@class='form-group']/input[@name='email']")).sendKeys(randomEmail);
+   let setComment = await driver.findElement(By.xpath("//div[@class='form-group']/textarea[@name='comment']"));
+   await setComment.sendKeys("TEST ORDER. This is a test purchase. Please don't confirm it.");
+
+
+   //Select a payment method
+    await driver.findElement(By.xpath("//input[@name='payment_method' and @value='online']")).click();
+    
+    //Select a delivery method
+    await driver.findElement(By.xpath("//input[@name='shipping_method' and @value='courier.express']")).click();
+
+   //await driver.findElement(By.xpath("//a[contains(text(),'Login to Avail')]")).click();
+      
+   await driver.sleep(1000);
+   await driver.quit();
+
+}
+
+testRun();
+
+ 
